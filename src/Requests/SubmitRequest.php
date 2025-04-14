7bff6e976c69062fa0708ff30258c84d8c2a85b6
Tschigo <tschigo@gmail.com>
2025-04-14 07:53:51 +0200

    Add Saloon PHP library and DbSchenker connector

---
+++ src/Requests/SubmitRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace SmartDato\DbSchenker\Requests;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class SubmitRequest extends Request
+{
+    /**
+     * The HTTP method of the request
+     */
+    protected Method $method = Method::GET;
+
+    /**
+     * The endpoint for the request
+     */
+    public function resolveEndpoint(): string
+    {
+        return '/example';
+    }
+}
